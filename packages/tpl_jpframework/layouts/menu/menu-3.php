aae2625dee9a0b8947e3525b285206dbb66add7c
kim <kim@aficat.com>
2016-09-23 18:08:03 +0200

    added menu 3

---
+++ packages/tpl_jpframework/layouts/menu/menu-3.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @version    	1.2.0 menu-1.php $ kim 2014
+ * @package		JP_Framework
+ * @copyright   Copyright © 2014 - All rights reserved.
+ * @license		GNU/GPL
+ * @author		kim
+ * @author mail kim@afi.cat
+ * @website		http://www.afi.cat
+ *
+ */
+
+?>
+
+<!-- menu 3 -->
+<div id="container">
+
+  	<div class="navT">
+    		<div class="icon"></div>
+  	</div>
+  
+  	<div id="menu">
+    		<jdoc:include type="modules" name="jpf-menu" />
+  	</div>
+
+</div>
