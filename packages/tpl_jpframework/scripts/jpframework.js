e8e9459a6e34a4b065e9b69d7d2b13738bb69324
kim <kim@aficat.com>
2016-10-29 12:35:57 +0200

    framework js fixes

--- packages/tpl_jpframework/scripts/jpframework.js
+++ packages/tpl_jpframework/scripts/jpframework.js
@@ -1,17 +1,18 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
+
 	// browser window scroll (in pixels) after which the "back to top" link is shown
 	var offset = 300,
-		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		offset_opacity = 1200,
-		//duration of the top scrolling animation (in ms)
-		scroll_top_duration = 700,
-		//grab the "back to top" link
-		$back_to_top = $('.cd-top');
+	//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+	offset_opacity = 1200,
+	//duration of the top scrolling animation (in ms)
+	scroll_top_duration = 700,
+	//grab the "back to top" link
+	$back_to_top = $('.cd-top');
 
 	//hide or show the "back to top" link
-	$(window).scroll(function(){
-		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-		if( $(this).scrollTop() > offset_opacity ) { 
+	jQuery(window).scroll(function(){
+		( jQuery(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( jQuery(this).scrollTop() > offset_opacity ) { 
 			$back_to_top.addClass('cd-fade-out');
 		}
 	});
@@ -19,26 +20,25 @@ jQuery(document).ready(function($){
 	//smooth scroll to top
 	$back_to_top.on('click', function(event){
 		event.preventDefault();
-		$('body,html').animate({
+		jQuery('body,html').animate({
 			scrollTop: 0 ,
 		 	}, scroll_top_duration
 		);
 	});
+	
+	//Performs a smooth page scroll to an anchor on the same page.
+	jQuery('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		  //if( jQuery(this).attr("href")=="#carousel-home") return;//This is the exception change the id
+		  var target = jQuery(this.hash);
+		  target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
+		  if (target.length) {
+		    jQuery('html,body').animate({
+		      scrollTop: target.offset().top
+		    }, 1000);
+		    return false;
+		  }
+		}
+	});
 
-});
-//Performs a smooth page scroll to an anchor on the same page.
-jQuery(function() {
-  jQuery('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-    	//if( jQuery(this).attr("href")=="#carousel-home") return;//This is the exception change the id
-      var target = jQuery(this.hash);
-      target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        jQuery('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
 });
