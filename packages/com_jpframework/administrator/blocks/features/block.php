2fa2db0d648ea791243c8ae82c59956f7d098984
kim <kim@aficat.com>
2018-10-30 20:02:14 +0100

    update server

--- packages/com_jpframework/administrator/blocks/features/block.php
+++ packages/com_jpframework/administrator/blocks/features/block.php
@@ -1,60 +1,56 @@
 <?php
 
 /**
- * @version     1.0.0
- * @package     com_jpframework
- * @copyright   Copyright (C) 2015. Todos los derechos reservados.
- * @license     Licencia Pública General GNU versión 2 o posterior. Consulte LICENSE.txt
- * @author      aficat <kim@aficat.com> - http://www.afi.cat
- */
-// No direct access
-defined('_JEXEC') or die;
+* @version		$Id: com_jpframework  Kim $
+* @package		com_jpframework v 1.0.0
+* @copyright	Copyright (C) 2014 Afi. All rights reserved.
+* @license		GNU/GPL, see LICENSE.txt
+*/
 
-$blockid 	= JRequest::getVar('blockid');
-$items 		= json_decode(blocksHelper::getBlockParameter($blockid, 'list_features'), true);
-$heading 	= blocksHelper::getBlockParameter($blockid,'feature_heading');
-$uniqid  	= blocksHelper::getBlockParameter($blockid, 'uniqid', 'block-'.$blockid);
-$pos 		= blocksHelper::getBlockParameter($blockid,'content_position', '');
+// restricted access
+defined('_JEXEC') or die('Restricted access');
 
+$blockid = JRequest::getVar('blockid');
+$cid 	= blocksHelper::getBlockParameter($blockid,'uniqid');
+$items 	= json_decode(blocksHelper::getBlockParameter($blockid, 'list_features'), true);
+$height = blocksHelper::getBlockParameter($blockid, 'height', '140');
+$subheading = blocksHelper::getBlockParameter($blockid, 'feature_subheading');
+$data   = blocksHelper::group_by_key($items);
 ?>
 
-<section id="<?= $uniqid; ?>" style="background-color:<?= blocksHelper::getBlockParameter($blockid,'block_color'); ?>;color:<?= blocksHelper::getBlockParameter($blockid,'block_font_color'); ?>">
+<section id="<?= $uniqid; ?>">
 
-	<div class="container jpfblock marketing">
+<div class="container marketing">
 
-		<?php if($heading != '') : ?>
-		<header>
-		<h1><?= $heading; ?></h1>
-		<p class="lead"><?= blocksHelper::getBlockParameter($blockid,'feature_subheading'); ?></p>
-		</header>
+	<header>
+		<h1><?= blocksHelper::getBlockParameter($blockid, 'feature_heading'); ?></h1>
+		<?php if($subheading) : ?>
+		<p><?= $subheading; ?></p>
 		<?php endif; ?>
+	</header>
 
-		<div class="row">
+	<div class="row">
 
-			<?php 
-		  	$i = 0;
-		  	foreach($items as $item): 
-		  	?>
-			<!-- Feature Item <?= $i; ?> -->
-			<div class="col-lg-4 <?= $pos; ?>">
-				<div class="featurette-divider">
-				<img class="rounded-circle" src="<?= $items['feature_img'][$i]; ?>" alt="<?= $items['feature_title'][$i]; ?>">
-				</div>
-				<h4><?= $items['feature_title'][$i]; ?></h4>
-				<hr class="featurette-divider">
-				<p>
-				<?= $items['feature_text'][$i]; ?><br/>
-				</p>
-				<?php if($items['feature_btn'][$i] != '') : ?>
-				<a href="<?= $items['feature_btn_link'][$i]; ?>" role="button" class="btn btn-secondary"><?= $items['feature_btn'][$i]; ?></a>
-				<?php endif; ?>
-			</div>
-			<?php 
-			$i++;
-			endforeach; ?>
+		<?php 
+	  	$i = 0;
+	  	foreach($data as $k => $v):
+		if($v[2] == '') break;
+	  	?>
+		<div class="col-lg-4">
+		    <img class="rounded-circle" src="<?= $v[2]; ?>" alt="<?= $v[0]; ?>" height="<?= $height; ?>">
+		    <h2><?= $v[0]; ?></h2>
+		    <p><?= $v[1]; ?></p>
+		    <?php if($v[3] != '') : ?>
+		    <p><a class="btn btn-secondary" href="<?= $v[4]; ?>" role="button"><?= $v[3]; ?></a></p>
+		    <?php endif; ?>
+		</div>
+		<?php 
+		$i++;
+		endforeach; ?>
+	  </div> 
+		  
+	</div>
 
-		</div><!-- /row -->
-		
-	</div><!-- /container -->
+</div>
 
 </section>
