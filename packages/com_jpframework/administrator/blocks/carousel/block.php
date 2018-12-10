cdb276293f361553f32cc8da0a748562910cab07
kim <kim@aficat.com>
2018-12-10 09:54:27 +0100

    carousel block indicators config

--- packages/com_jpframework/administrator/blocks/carousel/block.php
+++ packages/com_jpframework/administrator/blocks/carousel/block.php
@@ -15,6 +15,8 @@ blocksHelper::loadCss(JURI::root().'administrator/components/com_jpframework/blo
 blocksHelper::getBlockParameter($blockid,'carousel_play') == 0 ? $play = "false" : $play = blocksHelper::getBlockParameter($blockid,'carousel_play');
 $cid 	= blocksHelper::getBlockParameter($blockid,'carousel_id');
 $items 	= json_decode(blocksHelper::getBlockParameter($blockid, 'list_images'), true);
+$controls = blocksHelper::getBlockParameter($blockid, 'carousel_controls', 1);
+$indicators = blocksHelper::getBlockParameter($blockid, 'carousel_indicators', 1);
 $height = blocksHelper::getBlockParameter($blockid, 'carousel_height');
 $data   = blocksHelper::group_by_key($items);
 //print_r($data);
@@ -48,6 +50,15 @@ endforeach; ?>
   
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
+  
+  	<?php if(count($data) > 1 && $indicators == 1) : ?>
+    <ol class="carousel-indicators">
+    	<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    	<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    	<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  	</ol>
+  	<?php endif; ?>
+  
   	<?php 
   	$i = 0;
   	foreach($data as $k => $v):
@@ -66,7 +77,7 @@ endforeach; ?>
     endforeach; ?>
   </div> 
   
-  <?php if(count($data) > 1) : ?>
+  <?php if(count($data) > 1 && $controls == 1) : ?>
   <!-- Controls -->
   <a class="left carousel-control" href="#<?= $cid; ?>" data-slide="prev">
     <span class="glyphicon glyphicon-chevron-left"></span>
