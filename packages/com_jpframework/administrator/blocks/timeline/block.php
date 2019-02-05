3d97af79736917c51cb4d4df0898f7b7d7000560
kim <kim@aficat.com>
2019-02-05 17:01:47 +0100

    timeline fix

--- packages/com_jpframework/administrator/blocks/timeline/block.php
+++ packages/com_jpframework/administrator/blocks/timeline/block.php
@@ -14,51 +14,40 @@ require_once(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_jpframework' . 
 blocksHelper::loadCss(JURI::root().'administrator/components/com_jpframework/blocks/timeline/assets/css/timeline.css');
 blocksHelper::loadJs(JURI::root().'administrator/components/com_jpframework/blocks/timeline/assets/js/timeline.js');
 $blockid = JRequest::getVar('blockid');
+
+$parts 	= json_decode(blocksHelper::getBlockParameter($blockid, 'tm_parts'), true);
+$tm   = blocksHelper::group_by_key($parts);
 ?>
 
 <section id="<?= blocksHelper::getBlockParameter($blockid, 'uniqid', 'block-'.$blockid); ?>" style="background-color:<?= blocksHelper::getBlockParameter($blockid,'block_color'); ?>;color:<?= blocksHelper::getBlockParameter($blockid,'block_font_color'); ?>">
 <div class="container">
-    <div class="page-header text-center">
+
+    <?php if(blocksHelper::getBlockParameter($blockid, 'tm_title') != '') : ?>
+    <div class="page-header timeline-header text-center mb-5">
         <h1 id="timeline"><?= blocksHelper::getBlockParameter($blockid, 'tm_title'); ?></h1>
+        <?php if(blocksHelper::getBlockParameter($blockid, 'tm_subtitle') != '') : ?>
+        <p><?= blocksHelper::getBlockParameter($blockid, 'tm_subtitle'); ?></p>
+        <?php endif; ?>
     </div>
+    <?php endif; ?>
+    
     <ul class="timeline">
-        <li>
-          <div class="timeline-badge primary"><a><i class="fas fa-pencil-alt" rel="tooltip" title="11 hours ago via Twitter" id=""></i></a></div>
+    
+    	<?php foreach($tm as $k => $v) : ?>
+        <li  <?php if($v[0] == 'right') : ?>class="timeline-inverted"<?php endif; ?>>
+          <div class="timeline-badge primary"><a><i class="<?= $v[1]; ?>" rel="tooltip" title="<?= $v[1]; ?>"></i></a></div>
           <div class="timeline-panel">
             <div class="timeline-heading">
-              <img class="img-responsive" src="http://lorempixel.com/1600/500/sports/2" />
+              <img class="img-responsive" src="<?= $v[2]; ?>" />
               
             </div>
             <div class="timeline-body">
-              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+              <p><?= $v[3]; ?></p>
               
             </div>
           </div>
         </li>        
-        
-        <li  class="timeline-inverted">
-          <div class="timeline-badge primary"><a><i class="fas fa-pencil-alt" rel="tooltip" title="11 hours ago via Twitter" id=""></i></a></div>
-          <div class="timeline-panel">
-            <div class="timeline-heading">
-              <img class="img-responsive" src="http://lorempixel.com/1600/500/sports/2" />
-              
-            </div>
-            <div class="timeline-body">
-              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-              
-            </div>
-          </div>
-        </li>
-        
-        <li>
-          <div class="timeline-badge primary"><a><i class="fas fa-pencil-alt" rel="tooltip" title="11 hours ago via Twitter" id=""></i></a></div>
-          <div class="timeline-panel">
-            <div class="timeline-body">
-              <p><b>All the credits go to <a href="http://bootsnipp.com/rafamaciel">Rafamaciel</a></b></p>
-              <p>I only make it responsive and remove the empty spaces to be more like Facebook timeline!</p>
-            </div>
-          </div>
-        </li>
+        <?php endforeach; ?>             
         
         <li class="clearfix" style="float: none;"></li>
     </ul>
