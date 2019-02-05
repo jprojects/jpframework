a1f8ec8afe91d39afbe3f7a7487a06c160523e69
kim <kim@aficat.com>
2019-02-05 17:31:54 +0100

    news update

---
+++ packages/com_jpframework/administrator/blocks/news/assets/js/news.js
@@ -0,0 +1,16 @@
+
+jQuery(document).ready(function() {
+    jQuery("#news-slider2").owlCarousel({
+        items:3,
+        itemsDesktop:[1199,2],
+        itemsDesktopSmall:[980,2],
+        itemsMobile:[600,1],
+        pagination:false,
+        navigationText:false,
+        autoPlay:true
+    });
+});
+
+
+
+
--- packages/com_jpframework/administrator/blocks/news/block.php
+++ packages/com_jpframework/administrator/blocks/news/block.php
@@ -11,7 +11,9 @@
 defined('_JEXEC') or die('Restricted access');
 require_once('helper.php');
 $blockid = JRequest::getVar('blockid');
-blocksHelper::loadCss(JURI::root().'administrator/components/com_jpframework/blocks/SliderNews/assets/css/SliderNews.css');
+blocksHelper::loadCss(JURI::root().'administrator/components/com_jpframework/blocks/news/assets/css/news.css');
+blocksHelper::loadJs('https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js');
+blocksHelper::loadJs(JURI::root().'administrator/components/com_jpframework/blocks/news/assets/js/news.js');
 $cid = blocksHelper::getBlockParameter($blockid, 'carousel_id');
 $cat = blocksHelper::getBlockParameter($blockid, 'category');
 $uniqid = blocksHelper::getBlockParameter($blockid, 'uniqid');
@@ -23,31 +25,45 @@ $uniqid = blocksHelper::getBlockParameter($blockid, 'uniqid');
 </style>
 
 <section class="SliderNews" id="<?= blocksHelper::getBlockParameter($blockid, 'uniqid', 'block-'.$blockid); ?>">
-
-	<div class="row">
-		<div class="col col-xs-12">
-        	<div class="blog-grids" id="<?= $cid; ?>">
-                
-            	<?php foreach(SliderNewsHelper::getArticles($cat, 3) as $item) : ?>
-            	<?php $link = JRoute::_('index.php?option=com_content&view=article&id='.$item->id.'&catid='.$cat); ?>
-                <div class="grid">
-                    <div class="entry-media">
-                        <a href="<?= $link; ?>"><?= SliderNewsHelper::getImage($item->introtext); ?></a>
-                    </div>
-                    <div class="entry-body">
-                        <span class="cat">Notícies</span>
-                        <h3><a href="<?= $link; ?>"><?= $item->title; ?></a></h3>
-                        <p><?= SliderNewsHelper::getText($item->introtext); ?></p>
-                        <div class="read-more-date">
-                            <a href="<?= $link; ?>">Llegir més..</a>
-                            <span class="date"><?= date('M j', strtotime($item->created)); ?></span>
+    
+    <div class="demo">
+        <div class="container">
+            <h3 class="h3">News slider Demo - 2 </h3>    
+            <div class="row">
+                <div class="col-md-12">
+                    <div id="news-slider2" class="owl-carousel">
+                    
+                    	<?php foreach(SliderNewsHelper::getArticles($cat, 3) as $item) : ?>
+                    	<?php $link = JRoute::_('index.php?option=com_content&view=article&id='.$item->id.'&catid='.$cat); ?>
+                        <div class="post-slide2">
+                            <div class="post-img">
+                                <a href="<?= $link; ?>"><?= SliderNewsHelper::getImage($item->introtext); ?></a>
+                            </div>
+                            <div class="post-content">
+                                <h3 class="post-title"><a href="<?= $link; ?>"><?= $item->title; ?></a></h3>
+                                <p class="post-description">
+                                    <?= SliderNewsHelper::getText($item->introtext); ?>
+                                </p>
+                                <ul class="post-bar">
+                                    <li><i class="fa fa-calendar"></i> <?= date('M j', strtotime($item->created)); ?></li>
+                                    <!-- add tabs in the future 
+                                    <li>
+                                        <i class="fa fa-folder"></i>
+                                        <a href="#">Mockup</a>
+                                        <a href="#">Graphics</a>
+                                        <a href="#">Flayers</a>
+                                    </li>
+                                    -->
+                                </ul>
+                                <a href="<?= $link; ?>" class="read-more">read more</a>
+                            </div>
                         </div>
+                        <?php endforeach; ?>
+                            
                     </div>
                 </div>
-				<?php endforeach; ?>
-				
             </div>
         </div>
-	</div>       
+    </div>     
 
 </section>
