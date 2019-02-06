563dae92da8d96cb31f45c6dac19d4da57825f61
kim <kim@aficat.com>
2019-02-06 16:27:33 +0100

    menu 5

---
+++ packages/tpl_jpframework/layouts/menu/menu-5.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @version    	1.2.0 menu-1.php $ kim 2014
+ * @package		JP_Framework
+ * @copyright   Copyright © 2014 - All rights reserved.
+ * @license		GNU/GPL
+ * @author		kim
+ * @author mail kim@afi.cat
+ * @website		http://www.afi.cat
+ *
+*/
+
+?>
+
+<script>
+jQuery(document).ready(function(){
+    function resizeNav() {
+        jQuery(".menu").css({"height": window.innerHeight});
+        var radius = Math.sqrt(Math.pow(window.innerHeight, 2) + Math.pow(window.innerWidth, 2));
+        var diameter = radius * 2;
+        jQuery(".nav-layer").width(diameter);
+        jQuery(".nav-layer").height(diameter);
+        jQuery(".nav-layer").css({"margin-top": -radius, "margin-left": -radius});      
+    }
+    jQuery(".nav-toggle").click(function() {
+        jQuery(".nav-toggle, .nav-layer, .menu").toggleClass("open");
+        if(jQuery(".nav-toggle").hasClass('open')) {
+        	jQuery(".logo").attr("src", "<?= JURI::base(); ?>images/logo_nyam.png");
+        } else {
+        	jQuery(".logo").attr("src", "<?= JURI::base(); ?>images/logo_nyam.png");
+        }
+    });
+    jQuery(window).resize(resizeNav);
+    resizeNav();
+    
+    //language into menu
+    var html = jQuery('#lang').html();
+    jQuery('.mod-list').prepend(html);
+});
+</script>
+
+<!-- menu 5 example: https://bootsnipp.com/snippets/35pvq -->
+<div class="topmenu fixed-top">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="navbar">
+                    <nav class="menu">
+                        <div class="nav-layer"></div>
+                        <div id="lang" style="display:none;"><jdoc:include type="modules" name="jpf-lang" /></div>
+                        <jdoc:include type="modules" name="jpf-menu" />
+                        
+                    </nav>
+                    <img src="images/logo_nyam.png" alt="Nyam" class="img-fluid logo clickable">
+                    <a class="nav-toggle">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
