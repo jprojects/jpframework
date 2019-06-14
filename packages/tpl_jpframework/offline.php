cc146df23472bdb030430c87328d6a058ef619b0
kim <kim@aficat.com>
2019-06-14 11:41:51 +0200

    offline page fixes

--- packages/tpl_jpframework/offline.php
+++ packages/tpl_jpframework/offline.php
@@ -27,60 +27,63 @@ $date   = $params->get( 'offline_date' );
 <meta charset="utf-8">
 <title><?= htmlspecialchars($app->getCfg('sitename')); ?></title>
 <meta http-equiv="X-UA-Compatible" content="chrome=1">
-<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/open-sans-y2k" type="text/css"/>  
-<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/lobster" type="text/css"/>  
-<!-- forkawesome -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.1.5/css/fork-awesome.min.css" integrity="sha256-P64qV9gULPHiZTdrS1nM59toStkgjM0dsf5mK/UwBV4=" crossorigin="anonymous">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/montserrat" type="text/css"/>   
 <!-- custom css -->
 <link href="<?= $this->baseurl; ?>/templates/<?= $this->template; ?>/offline/css/custom.css" rel="stylesheet"/>
 </head>
 
+<style>
+	#home { background-image: url(../<?= $app->getCfg('offline_image'); ?>); }
+</style>
+
 <body id="home">
-<div id="Header">
-<div class="wrapper">
-	<h1><?= htmlspecialchars($app->getCfg('sitename')); ?></h1>	
+	<div id="Header">
+		<div class="wrapper">
+			<h1><?= htmlspecialchars($app->getCfg('sitename')); ?></h1>	
+		</div>
 	</div>
-</div>
-<div id="Content" class="wrapper"> 
-<div class="countdown styled"></div> 
-<?php if ($app->getCfg('display_offline_message', 1) == 1 && str_replace(' ', '', $app->getCfg('offline_message')) != ''): ?>
-    <h1 class="intro"><?= $app->getCfg('offline_message'); ?></h1>
-<?php elseif ($app->getCfg('display_offline_message', 1) == 2 && str_replace(' ', '', JText::_('JOFFLINE_MESSAGE')) != ''): ?>
-    <h1 class="intro"><?= JText::_('JOFFLINE_MESSAGE'); ?></h1>
-<?php  endif; ?></h2>
-<div id="subscribe"> 
-	<form action="" method="post" onsubmit="">
-		<p><input name="" value="Enter your e-mail" type="text" id=""/>
-		<input type="button" value="Submit"/></p>
-	</form>
-	<div id="socialIcons">
-		<ul> 
-			<li><a href="<?= $params->get('youtube', '#'); ?>#"><i class="fa fa-youtube"></i></a></li>
-          	<li><a href="<?= $params->get('facebook', '#'); ?>"><i class="fa fa-facebook-official"></i></a></li>
-          	<li><a href="<?= $params->get('twitter', '#'); ?>"><i class="fa fa-twitter"></i></a></li>
-		  	<li><a href="<?= $params->get('instagram', '#'); ?>"><i class="fa fa-instagram"></i></a></li>
-		</ul>
+	<div id="Content" class="wrapper"> 
+		<div class="countdown styled"></div> 
+		<?php if ($app->getCfg('display_offline_message', 1) == 1 && str_replace(' ', '', $app->getCfg('offline_message')) != ''): ?>
+			<h1 class="intro"><?= $app->getCfg('offline_message'); ?></h1>
+		<?php elseif ($app->getCfg('display_offline_message', 1) == 2 && str_replace(' ', '', JText::_('JOFFLINE_MESSAGE')) != ''): ?>
+			<h1 class="intro"><?= JText::_('JOFFLINE_MESSAGE'); ?></h1>
+		<?php  endif; ?></h2>
+		<div id="subscribe" class="col-6 mx-auto">
+    	<form action="<?= JRoute::_('index.php', true); ?>" method="post" id="form-login">
+		<div class="form-group">
+			<input class="form-control mx-auto" name="username" id="username" type="text" placeholder="<?= JText::_('JGLOBAL_USERNAME') ?>" />
+		</div>
+		<div class="form-group">
+			<input class="form-control mx-auto" type="password" name="password" placeholder="<?= JText::_('JGLOBAL_PASSWORD') ?>" id="passwd" />
+		</div>
+			<input type="submit" name="Submit" class="btn btn-default btn-block mx-auto" value="<?= JText::_('JLOGIN') ?>" />
+		    <input type="hidden" name="option" value="com_users" />
+		    <input type="hidden" name="task" value="user.login" />
+		    <input type="hidden" name="return" value="<?= base64_encode(JURI::base()) ?>" />
+		    <?= JHtml::_('form.token'); ?>
+    	</form>
 	</div>
-</div>
-<span class="tempBy">Copyright &copy; 2016 <?= htmlspecialchars($app->getCfg('sitename')); ?> by <a target="_blank" style="color:#fff;" href="https://www.afi.cat">Afi informàtica</a></span>
-</div>
+		<span class="tempBy">Copyright &copy; 2016 <?= htmlspecialchars($app->getCfg('sitename')); ?> by <a target="_blank" style="color:#fff;" href="https://www.afi.cat">Afi informàtica</a></span>
+	</div>
 
-<div id="overlay"></div>
+	<div id="overlay"></div>
 
-<!--Scripts-->
-<script
-src="https://code.jquery.com/jquery-1.12.4.min.js"
-integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
-crossorigin="anonymous"></script>
-<script type="text/javascript" src="<?= $this->baseurl; ?>/templates/<?= $this->template; ?>/offline/js/Backstretch.js"></script>
-<script type="text/javascript" src="<?= $this->baseurl; ?>/templates/<?= $this->template; ?>/offline/js/jquery.countdown.js"></script>
-<script>
-$( function() {
-	$.backstretch("<?= JURI::root().$app->get('offline_image', '<?= $this->baseurl; ?>/templates/<?= $this->template; ?>/offline/images/offline.jpg'); ?>"); //background image
-	var endDate = "<?= $date; ?>"; //countdown
-});
-</script>
-<script type="text/javascript" src="js/scripts.js"></script>
+	<!--Scripts-->
+	<script
+	src="https://code.jquery.com/jquery-1.12.4.min.js"
+	integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+	crossorigin="anonymous"></script>
+	<script type="text/javascript" src="<?= $this->baseurl; ?>/templates/<?= $this->template; ?>/offline/js/Backstretch.js"></script>
+	<script type="text/javascript" src="<?= $this->baseurl; ?>/templates/<?= $this->template; ?>/offline/js/jquery.countdown.js"></script>
+	<script>
+	$( function() {
+		$.backstretch("<?= JURI::root().$app->get('offline_image', '<?= $this->baseurl; ?>/templates/<?= $this->template; ?>/offline/images/offline.jpg'); ?>"); //background image
+		var endDate = "<?= $date; ?>"; //countdown
+	});
+	</script>
+	<script type="text/javascript" src="js/scripts.js"></script>
 
 </body>
 </html>
