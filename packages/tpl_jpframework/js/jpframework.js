0a732dc2527c59c08208520838fa14410e776e2e
kim <kim@aficat.com>
2020-05-04 16:19:42 +0200

    new js functions

--- packages/tpl_jpframework/js/jpframework.js
+++ packages/tpl_jpframework/js/jpframework.js
@@ -28,6 +28,31 @@ jQuery(document).ready(function($) {
 		 	}, scroll_top_duration
 		);
 	});
+
+	//Replace all SVG images with inline SVG
+	jQuery('img.svg').each(function(){
+		var $img = jQuery(this);
+		var imgID = $img.attr('id');
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+		jQuery.get(imgURL, function(data) {
+		    // Get the SVG tag, ignore the rest
+		    var $svg = jQuery(data).find('svg');
+		    // Add replaced image's ID to the new SVG
+		    if(typeof imgID !== 'undefined') {
+		        $svg = $svg.attr('id', imgID);
+		    }
+		    // Add replaced image's classes to the new SVG
+		    if(typeof imgClass !== 'undefined') {
+		        $svg = $svg.attr('class', imgClass+' replaced-svg');
+		    }
+		    // Remove any invalid XML tags as per http://validator.w3.org
+		    $svg = $svg.removeAttr('xmlns:a');
+		    // Replace image with new SVG
+		    $img.replaceWith($svg);
+
+		}, 'xml');
+	});
 	
 	jQuery('.tos').click(function() {
 		if(jQuery(this).is(':checked')) {  
@@ -44,6 +69,17 @@ jQuery(document).ready(function($) {
             jQuery('.newsletter').val(0);  
         } 
 	});
+
+	jQuery('.lang-es').click(function(e){
+    	e.preventDefault();
+		var href = jQuery('#es').attr('data-href');
+		document.location.href = href;
+    });
+    jQuery('.lang-ca').click(function(e){
+    	e.preventDefault();
+		var href = jQuery('#ca').attr('data-href');
+		document.location.href = href;
+    });
 	
 	setTimeout(function() {
     $('.message-container').fadeOut('fast');
