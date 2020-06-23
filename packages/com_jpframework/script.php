fe6712d8b069cdb2fc48fcc1164e6b986e5a737e
jprojects <kim@aficat.com>
2020-06-23 11:12:20 +0200

    fix script install

--- packages/com_jpframework/script.php
+++ packages/com_jpframework/script.php
@@ -33,9 +33,9 @@ class com_jpframeworkInstallerScript
 		$post->state 	= 1;
 		$post->catid 	= 2;
 		$post->created 	= date('Y-m-d H:i:s');
-		$post->created_by = 928;
+		//$post->created_by = 928;
     $post->modified 	= date('Y-m-d H:i:s');
-    $post->modified_by = 928;
+    //$post->modified_by = 928;
     $post->checked_out_time 	= date('Y-m-d H:i:s');
     $post->publish_up 	= date('Y-m-d H:i:s');
     $post->images 	= '';
